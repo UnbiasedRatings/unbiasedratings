5867bca8941ead465d8d02e106fd63b4ad17519b
Mohneesh <mohneeshgarg2000@gmail.com>
2024-10-05 14:42:36 +0530

    Unbiased Rating project setup

---
+++ src/test/java/com/platform/unbiasedratings/UnbiasedratingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platform.unbiasedratings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnbiasedratingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
